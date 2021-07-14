b1be392687247ebbcd569b7ef550fa324709e05d
tanakaasuka <1585143493@qq.com>
2021-07-14 11:55:29 +0800

    添加store

--- src/main.js
+++ src/main.js
@@ -6,11 +6,14 @@ import "normalize.css";
 import router from './router'
 import "vue-toastification/dist/index.css";
 import Toast from "vue-toastification";
+import store from './store/store'
+
 
 
 
 let app=createApp(App)
 app.use(router)
+app.use(store)
 app.use(Toast,{
   position:"top-right",
   transition: "Vue-Toastification__fade",
