bb1f66ddf9a8acce8a2790cc2364ba065f1739a3
tanakaasuka <1585143493@qq.com>
2021-07-14 16:17:03 +0800

    添加一些注释

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,10 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/about/About.vue')
   },
   {
+    // 如果没登陆，跳去登录页
+    // 如果登录，看下是不是管理员
+    // 如果不是管理员，则跳回首页
+    // 如果是管理员，则进入路由
     path: '/upload',
     name: 'Upload',
     // route level code-splitting
