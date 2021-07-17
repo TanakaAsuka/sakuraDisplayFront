4c9fa6e7c7b0b76463fba9001fd3706e76db3320
tanakaasuka <1585143493@qq.com>
2021-07-17 09:58:21 +0800

    删掉一些注释

--- src/views/gallery/Gallery.vue
+++ src/views/gallery/Gallery.vue
@@ -14,100 +14,15 @@
         :data-lg-size="img.widthAndHeight"
         class="gallery-item grid-item"
         :data-src="img.url"
-        data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@entrycube' >Diego Guzmán </a></h4> <p> Location - <a href='https://unsplash.com/s/photos/fushimi-inari-taisha-shrine-senbontorii%2C-68%E7%95%AA%E5%9C%B0-fukakusa-yabunouchicho%2C-fushimi-ward%2C-kyoto%2C-japan'>Fushimi Ward, Kyoto, Japan</a></p>"
+        data-sub-html="
+        <h4>
+          
+        </h4>
+           "
       >
         <img class="img-responsive" :src="img.url" />
       </a>
-      <!-- <a
-        data-lg-size="1400-1400"
-        class="gallery-item grid-item"
-        data-src="https://images.unsplash.com/photo-1544550285-f813152fb2fd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"
-        data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@asoshiation' >Shah </a></h4><p> Location - <a href='https://unsplash.com/s/photos/shinimamiya%2C-osaka%2C-japan'>Shinimamiya, Osaka, Japan</a></p>"
-      >
-        <img
-          class="img-responsive"
-          src="https://images.unsplash.com/photo-1544550285-f813152fb2fd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80"
-        />
-      </a>
-      <a
-        data-lg-size="1400-1400"
-        class="gallery-item grid-item"
-        data-src="https://images.unsplash.com/photo-1584592740039-cddf0671f3d4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"
-        data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@katherine_xx11' >Katherine Gu </a></h4><p> For all those years we were alone and helpless.</p>"
-      >
-        <img
-          class="img-responsive"
-          src="https://images.unsplash.com/photo-1584592740039-cddf0671f3d4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80"
-        />
-      </a>
 
-      <a
-        data-lg-size="1496-720"
-        class="gallery-item grid-item"
-        :data-src="require('../../../public/assets/1.jpg')"
-        data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@katherine_xx11' >Katherine Gu </a></h4><p> For all those years we were alone and helpless.</p>"
-      >
-        <img class="img-responsive" src="../../../public/assets/1.jpg" />
-      </a>
-
-      <a
-        data-lg-size="1617-1103"
-        class="gallery-item grid-item"
-        :data-src="require('../../../public/assets/2.jpg')"
-        data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@katherine_xx11' >Katherine Gu </a></h4><p> For all those years we were alone and helpless.</p>"
-      >
-        <img class="img-responsive" src="../../../public/assets/2.jpg" />
-      </a>
-      <a
-        data-lg-size="6500-3517"
-        class="gallery-item grid-item"
-        :data-src="require('../../../public/assets/3.jpg')"
-        data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@katherine_xx11' >Katherine Gu </a></h4><p> For all those years we were alone and helpless.</p>"
-      >
-        <img class="img-responsive" src="../../../public/assets/3.jpg" />
-      </a>
-      <a
-        data-lg-size="1058-1500"
-        class="gallery-item grid-item"
-        :data-src="require('../../../public/assets/4.jpg')"
-        data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@katherine_xx11' >Katherine Gu </a></h4><p> For all those years we were alone and helpless.</p>"
-      >
-        <img class="img-responsive" src="../../../public/assets/4.jpg" />
-      </a>
-      <a
-        data-lg-size="1054-1500"
-        class="gallery-item grid-item"
-        :data-src="require('../../../public/assets/5.jpg')"
-        data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@katherine_xx11' >Katherine Gu </a></h4><p> For all those years we were alone and helpless.</p>"
-      >
-        <img class="img-responsive" src="../../../public/assets/5.jpg" />
-      </a>
-      <a
-        data-lg-size="1500-938"
-        class="gallery-item grid-item"
-        :data-src="require('../../../public/assets/6.jpg')"
-        data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@katherine_xx11' >Katherine Gu </a></h4><p> For all those years we were alone and helpless.</p>"
-      >
-        <img class="img-responsive" src="../../../public/assets/6.jpg" />
-      </a>
-
-      <a
-        data-lg-size="1500-1211"
-        class="gallery-item grid-item"
-        :data-src="require('../../../public/assets/7.jpg')"
-        data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@katherine_xx11' >Katherine Gu </a></h4><p> For all those years we were alone and helpless.</p>"
-      >
-        <img class="img-responsive" src="../../../public/assets/7.jpg" />
-      </a>
-
-      <a
-        data-lg-size="1500-1169"
-        class="gallery-item grid-item"
-        :data-src="require('../../../public/assets/8.jpg')"
-        data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@katherine_xx11' >Katherine Gu </a></h4><p> For all those years we were alone and helpless.</p>"
-      >
-        <img class="img-responsive" src="../../../public/assets/8.jpg" />
-      </a> -->
     </lightgallery>
   </div>
 </template>
@@ -196,7 +111,19 @@ body {
 .grid-item {
   width: 300px;
   margin: 5px;
+
+  .lg-sub-html {
+  display: flex;
+  h4 {
+    flex: 1;
+  }
+  div {
+    width: 120px;
+  }
 }
+}
+// 底部html
+
 
 .lightgallery-vue {
   width: 82vw;
